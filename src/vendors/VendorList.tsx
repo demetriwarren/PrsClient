e4985ad5d2836b2e8d975b7953b09cd62cbd6321
Demetri Warren <dwarren@tql.com>
2024-08-16 14:32:20 -0400

    added toast and VendorList code

--- src/vendors/VendorList.tsx
+++ src/vendors/VendorList.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 
 import { vendorAPI } from "./VendorApi";
 import { Vendor } from "./Vendor";
+import { VendorCard } from "./VendorCard";
+import toast from "react-hot-toast";
 
 //gonna have the useState of vendors/useForm in here
 //need the busy state,
@@ -27,5 +29,30 @@ export function VendorList() {
     loadVendors();
   }, []);
 
-  
+  async function remove(vendor: Vendor){
+    if(confirm("Are you sure you want to delete this vendor?"))
+        if(vendor.id){
+            await vendorAPI.delete(vendor.id);
+            let updatedVendors = vendors.filter((v) => v.id !== vendor.id);
+            setVendors(updatedVendors);
+            toast.success("Successfully deleted.");
+        }
+  }
+
+  return (
+    <>
+        {busy && (
+            <section className="d-flex justify-content-center align-items-center align-content-center vh-100">
+                <div className="spinner-border text-primary" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </section>
+        )}
+        <section className="d-flex flex-wrap gap-2">
+            {vendors.map((vendor) => (
+                <VendorCard key={vendor.id} vendor={vendor} onRemove={remove}/>
+            ))}
+        </section>
+    </>
+  )
 }
