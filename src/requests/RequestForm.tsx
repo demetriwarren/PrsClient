6a498d8c3ef6c1d4d1b33a36cdd2f60cb1559289
Demetri Warren <dwarren@tql.com>
2024-08-21 14:10:33 -0400

    Request forms functioning. (Still need status and requestedBy to be disabled)

--- src/requests/RequestForm.tsx
+++ src/requests/RequestForm.tsx
@@ -1,13 +1,17 @@
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Request } from "./Request";
 import { requestAPI } from "./RequestApi";
 import { SubmitHandler, useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+import { User } from "../users/User"
+import { useState } from "react";
+import { userAPI } from "../users/UserApi";
 
 export function RequestForm() {
   const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
   const requestId = Number(id);
+  const [users, setUsers] = useState<User[]>([]);
 
   const {
     register,
@@ -15,8 +19,11 @@ export function RequestForm() {
     formState: { errors },
   } = useForm<Request>({
     defaultValues: async () => {
+        let usersData = await userAPI.list();
+        setUsers(usersData);
+
       if (!requestId) {
-        return Promise.resolve(new Request());
+        return Promise.resolve(new Request({userId: 61}));
       } else {
         return await requestAPI.find(requestId);
       }
@@ -39,60 +46,80 @@ export function RequestForm() {
   };
   return (
     <>
-      <form className="w-75" onSubmit={handleSubmit(save)}>
-        
-        
-
-        <div className="leftRow ">
-
-        <div className=" w-50">
-            <label htmlFor="description">Description</label>
-            <input className={`form-control ${errors.description && "is-invalid"}`} type="text" {...register("description", {required:"Description is required"})} placeholder="Enter a brief description for your purchase request"/>
-            <div className="invalid-feedback">{errors?.description?.message}</div>
-        </div>
-        <br />
-        <div className="w-50">
-            <label htmlFor="justification">Justification</label>
-            <input className={`form-control ${errors.justification && "is-invalid"}`} type="text" {...register("justification", {required:"Justification is required"})} placeholder="Enter a brief justification for your purchase request"/>
-            <div className="invalid-feedback">{errors?.justification?.message}</div>
-        </div>
-        </div>
-        <br />
-        <div className="row-3 w-50">
-            
-            <div>
-            <label htmlFor="deliveryMethod">Delivery Method</label>
-            <select className="form-select" id="deliveryMethod">
-                <option value="">Select Delivery Method</option>
-            </select>
+      <form className=" " onSubmit={handleSubmit(save)}>
+        <div className="d-flex ">
+          <section className="box1 container">
+            <div className="w-75">
+              <label htmlFor="description">Description</label>
+              <input
+                className={`form-control ${errors.description && "is-invalid"} mt-2`}
+                type="text"
+                {...register("description", { required: "Description is required" })}
+                placeholder="Enter a brief description for your purchase request"
+              />
+              <div className="invalid-feedback">{errors?.description?.message}</div>
+              <br />
             </div>
-            
-            <div>
 
-            <label htmlFor="Status">Status</label>
-            <select className="form-select" id="deliveryMethod">
-                <option value="">Select Status</option>
-            </select>
+            <div className="w-75">
+              <label htmlFor="justification">Justification</label>
+              <input
+                className={`form-control ${errors.justification && "is-invalid"} mt-2`}
+                type="text"
+                {...register("justification", { required: "Justification is required" })}
+                placeholder="Enter a brief justification for your purchase request"
+              />
+              <div className="invalid-feedback">{errors?.justification?.message}</div>
             </div>
-            
-            <div >
+          </section>
 
-            <label htmlFor="requestedBy">Requested By</label>
-            <select className="form-select" id="deliveryMethod">
-            <option value="">Select Requested By</option></select>
+          <section className="box2 container">
+            <div className="w-50">
+              <label htmlFor="deliveryMode">Delivery Method</label>
+              <select
+                className={`form-select mt-2 ${errors.deliveryMode && "is-invalid"}`} 
+                id="deliveryMode"
+                {...register("deliveryMode", { required: "Delivery method is required" })}
+              >
+                <option value="Pickup">Pickup</option>
+                <option value="Delivery">Delivery</option>
+                <option value="Signature Delivery">Signature Delivery</option>
+              </select>
+              <br />
             </div>
-                
 
+            <div className="w-50">
+              <label htmlFor="Status">Status</label>
+              <select
+                className={`form-select mt-2 ${errors.status && "is-invalid"}`}
+                id="deliveryMethod"
+                {...register("status", { required: "Status is required" })}
+              >
+                <option value="">Select...</option>
+                <option value="NEW">New</option>
+                <option value="REVIEW">Review</option>
+                <option value="APPROVED">Approved</option>
+                <option value="REJECTED">Rejected</option>
+              </select>
+              <br />
+            </div>
 
+            <div className="w-50">
+              <label htmlFor="requestedBy">Requested By</label>
+              <select className="form-select mt-2" id="deliveryMethod">
+                <option value=""></option>
+                {users.map((user) => (
+                  <option key={user.id} value={user.id}>
+                    {user.firstname} {user.lastname}
+                  </option>
+                ))}
+              </select>
+              <br />
+            </div>
 
-
-
+          </section>
         </div>
-
-
-
-
-        <div className="row-4 d-flex justify-content-end">
+        <div className="row-4 d-flex justify-content-end w-75">
           <a className="btn btn-outline-primary" href="/requests">
             Cancel
           </a>
--- src/requests/RequestTable.tsx
+++ src/requests/RequestTable.tsx
@@ -1,7 +1,68 @@
-export function RequestTable() {
+import { useEffect, useState } from "react";
+import { requestAPI } from "./RequestApi";
+import { Request } from "./Request";
+import toast from "react-hot-toast";
+import RequestTableRow from "./RequestTableRow";
+
+function RequestTable() {
+  const [requests, setRequests] = useState<Request[]>([]);
+  const [busy, setBusy] = useState(false);
+
+  async function loadRequest() {
+    try {
+      setBusy(true);
+      const data = await requestAPI.list();
+      setRequests(data);
+    } catch (error: any) {
+      toast.error(error.message);
+    } finally {
+      setBusy(false);
+    }
+  }
+
+  useEffect(() => {
+    loadRequest();
+  }, []);
+
+  async function remove(request: Request) {
+    if (confirm("Are you sure you want to delete this request?")) {
+      if (request.id) {
+        await requestAPI.delete(request.id);
+        let updatedRequests = requests.filter((r) => r.id !== request.id);
+        setRequests(updatedRequests);
+        toast.success("Successfully deleted.");
+      }
+    }
+  }
+
   return (
     <>
-      <p>List of requests will be here</p>
+      {busy && (
+        <section className="d-flex justify-content-center align-items-center align-content-center vh-100">
+          <div className=" spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        </section>
+      )}
+      <table className="table table-hover w-75">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Genre</th>
+            <th>Rating</th>
+            <th>Director</th>
+            <th>Budget</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+            <MovieTableRow key={movie.id} movie={movie} onRemove={remove} />
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
+
+export default MovieTable;
